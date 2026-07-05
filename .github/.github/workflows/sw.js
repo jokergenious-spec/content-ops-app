c3ec7eda0bdec86613901a2a64b5f503ac7189fa
jokergenious-spec <jokergenious@gmail.com>
2026-07-06 00:09:41 +0530

    Add files via upload

---
+++ .github/.github/workflows/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'content-ops-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(e.request, copy));
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
