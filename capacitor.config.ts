a1128e73ceb23280d1e80af81ea09e790c1f665c
jokergenious-spec <jokergenious@gmail.com>
2026-07-06 00:14:06 +0530

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jokergenious.creatorhub',
+  appName: 'Creator Hub',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
